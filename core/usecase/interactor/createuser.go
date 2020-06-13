a888e6f074aeab6bf080173e0b7172b0fadfe892
Douglas Rodrigues <douglas.rodrigues@dito.com.br>
2020-06-12 21:58:07 -0300

    adding copyright

--- core/usecase/interactor/createuser.go
+++ core/usecase/interactor/createuser.go
@@ -1,3 +1,7 @@
+// Copyright (c) 2020. Douglas Rodrigues - All rights reserved.
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
 package interactor
 
 import (
